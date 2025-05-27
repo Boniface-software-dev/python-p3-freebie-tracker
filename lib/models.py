5ec8a97b6f668473699f6939e577acac9ae91d28
Boniface Kimani Muguro <mmbonifacekimani@gmail.com>
2025-05-27 19:05:05 +0300

    first commit

--- lib/models.py
+++ lib/models.py
@@ -1,12 +1,15 @@
 from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
 
+# Define the naming convention for foreign keys
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 }
 metadata = MetaData(naming_convention=convention)
 
+# Create a base class for declarative models with the defined metadata
 Base = declarative_base(metadata=metadata)
 
 class Company(Base):
