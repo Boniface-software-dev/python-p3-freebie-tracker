c28b7fe099fc2b81fea02f4642c16a9ba724330b
Boniface Kimani Muguro <mmbonifacekimani@gmail.com>
2025-05-27 19:13:28 +0300

    Establish a relationship with Freebie

--- lib/models.py
+++ lib/models.py
@@ -16,8 +16,11 @@ class Company(Base):
     __tablename__ = 'companies'
 
     id = Column(Integer(), primary_key=True)
-    name = Column(String())
-    founding_year = Column(Integer())
+    name = Column(String(), nullable=False)
+    founding_year = Column(Integer(), nullable=False)
+
+    # Establish a relationship with Freebie
+    freebies = relationship('Freebie', backref='company', lazy='dynamic')
 
     def __repr__(self):
         return f'<Company {self.name}>'
