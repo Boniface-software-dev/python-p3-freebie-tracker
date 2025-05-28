e3b6649bc1dd522eca42437ce1c51f129f320e2f
Boniface Kimani Muguro <mmbonifacekimani@gmail.com>
2025-05-28 17:12:26 +0300

    debug.py set

--- lib/debug.py
+++ lib/debug.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 
 from sqlalchemy import create_engine
-
-from models import Company, Dev
+from models import Base, Company, Dev, Freebie
+from sqlalchemy.orm import sessionmaker
+# Create an engine and session
+engine = create_engine('sqlite:///freebies.db')
+Session = sessionmaker(bind=engine)
+session = Session()
 
 if __name__ == '__main__':
-    engine = create_engine('sqlite:///freebies.db')
-    import ipdb; ipdb.set_trace()
+    # Fetch all devs
+    devs = session.query(Dev).all()
+    for dev in devs:
+        print(dev)
+        print(f"Companies: {[company.name for company in dev.companies]}")
+        print(f"Received 'T-Shirt': {dev.received_one('T-Shirt')}")
